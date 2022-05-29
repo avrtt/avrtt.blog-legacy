188cf35bf6a85453548333d5fdcebc22dd98c34a
Vladislav <lenferdetroud@gmail.com>
2022-05-30 01:32:28 +0700

    Hash router test

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from "react-dom";
 import './index.css';
-import { BrowserRouter as Router, Routes, Route}
+import { HashRouter as Router, Routes, Route}
     from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';

87a76bcafada20feba705e236eb1e5e1b5a86d57
Vladislav <lenferdetroud@gmail.com>
2022-08-08 17:22:30 +0700

    Playlist added

--- src/pages/memories.js
+++ src/pages/memories.js
@@ -4,12 +4,19 @@ import { motion } from 'framer-motion';
 
 const TITLE = 'Memories - lenferdetroud.github.io'
 
-const iframeStyle = {
+const mapFrameStyle = {
 	border: 'none',
 	width: '100%',
 	height: '540px',
 	position: 'relative',
 	top: '-50px'
+}; 
+
+const playlistFrameStyle = {
+	display: 'block',
+	'margin-left': 'auto',
+	'margin-right': 'auto',
+	'margin-top': '100px'
 }; 
 
 const divStyle = {
@@ -30,10 +37,11 @@ const Memories = () => {
 			<title>{ TITLE }</title>
         </Helmet>
         
-        <iframe src="https://drive.google.com/embeddedfolderview?id=13x8Cs1TIWZSYN_ywbWZOT4FivWQ--8wG#grid" width="100%" height="700" frameborder="0" title='memories'></iframe>
+        <iframe src="https://drive.google.com/embeddedfolderview?id=13x8Cs1TIWZSYN_ywbWZOT4FivWQ--8wG#grid" width="100%" height="700" frameborder="0" title='drive'></iframe>
+        <iframe width='500' height='500' src='https://www.youtube.com/embed/videoseries?list=PLFzQnxF1skPuILXEWcVz-SJg0D5kzLHVv' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen style={playlistFrameStyle} title='playlist'></iframe>
         
         <div id="map-container" style={divStyle}>
-			<iframe id="map-embed" src="https://www.google.com/maps/d/embed?mid=1X9b0bo11WcY9on87ZQXoB3YUkccwnK49&ehbc=2E312F" style={iframeStyle} title="Travel Map" />
+			<iframe id="map-embed" src="https://www.google.com/maps/d/embed?mid=1X9b0bo11WcY9on87ZQXoB3YUkccwnK49&ehbc=2E312F" style={mapFrameStyle} title="Travel Map" />
 		</div>
         
     </motion.div>

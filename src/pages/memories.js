315400b54742a1f284f33daf94169099b3cae063
Vladislav <lenferdetroud@gmail.com>
2022-08-20 05:23:50 +0700

    -

--- src/pages/memories.js
+++ src/pages/memories.js
@@ -5,10 +5,12 @@ import { motion } from 'framer-motion';
 const TITLE = 'Memories - lenferdetroud.github.io'
 
 const playlistFrameStyle = {
-	display: 'block',
-	'margin-left': 'auto',
-	'margin-right': 'auto',
-	'margin-top': '100px'
+	
+}; 
+
+const playlistHolderStyle = {
+	'margin-top': '100px',
+	'text-align': 'center'
 }; 
 
 const Memories = () => {
@@ -24,7 +26,11 @@ const Memories = () => {
         </Helmet>
         
         <iframe src="https://drive.google.com/embeddedfolderview?id=13x8Cs1TIWZSYN_ywbWZOT4FivWQ--8wG#grid" width="100%" height="700" frameborder="0" title='drive'></iframe>
-        <iframe width='560' height='315' src='https://www.youtube.com/embed/videoseries?list=PLFzQnxF1skPuILXEWcVz-SJg0D5kzLHVv' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen style={playlistFrameStyle} title='playlist'></iframe>
+        <div style={playlistHolderStyle}>
+			<iframe width='560' height='315' src='https://www.youtube.com/embed/videoseries?list=PLFzQnxF1skPvlvnld01k0Pgmzxbn5OMx3' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen style={playlistFrameStyle} title='playlist'></iframe>
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+			<iframe width='560' height='315' src='https://www.youtube.com/embed/videoseries?list=PLFzQnxF1skPuILXEWcVz-SJg0D5kzLHVv' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen style={playlistFrameStyle} title='playlist'></iframe>
+        </div>
         
     </motion.div>
   );

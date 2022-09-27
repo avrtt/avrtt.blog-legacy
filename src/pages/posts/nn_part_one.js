8da265b4903930662aa04328338d40a52c5c85dc
Vladislav <lenferdetroud@gmail.com>
2022-09-27 19:53:59 +0700

    -

--- src/pages/posts/nn_part_one.js
+++ src/pages/posts/nn_part_one.js
@@ -4,19 +4,19 @@ import { motion } from 'framer-motion';
 import { itemData } from '../itemData';
 const TITLE = itemData.find(x => x.id === 1).title + ' - lenferdetroud.github.io'
 
-const Post1 = () => {
-  return (
-    <motion.div
-		initial={{opacity: 0 }}
-		animate={{opacity: 1 }}
-		exit={{opacity: 0 }}
-		transition={{ duration: 0.15 }}>
-		<Helmet><title>{ TITLE }</title></Helmet>
+const Post1 = ({ blockMap }) => {	
+	return (
+		<motion.div
+			initial={{opacity: 0 }}
+			animate={{opacity: 1 }}
+			exit={{opacity: 0 }}
+			transition={{ duration: 0.15 }}>
+			<Helmet><title>{ TITLE }</title></Helmet>
         
-        <p>Coming soon...</p>
+			<p>Test</p>
         
-    </motion.div>
-  );
+		</motion.div>
+	);
 };
   
 export default Post1;

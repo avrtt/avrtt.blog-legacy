45ff3249086552402bea7cbf097add0a340097a9
Vladislav <lenferdetroud@gmail.com>
2022-09-30 05:22:53 +0700

    -

--- src/pages/posts/ensembles.js
+++ src/pages/posts/ensembles.js
@@ -5,15 +5,13 @@ import { itemData } from '../itemData';
 import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 import TelegramComments from 'react-telegram-comments';
+import Frog from '../img/mysterious_frog.png';
 
 import pdf from "./sample.pdf";
 const postNumber = 2
 
 const TITLE = itemData.find(x => x.id === postNumber).title + ' - lenferdetroud.github.io';
 const keyString = itemData.find(x => x.id === postNumber).keyString;
-const pdfStyle = {
-	'zoom': '140%',
-}
 
 export default function Post() {  
 	
@@ -34,14 +32,24 @@ export default function Post() {
 			<Helmet><title>{ TITLE }</title></Helmet>
         	
 			<center>
-				<div style={pdfStyle}>
-					<Document file={pdf} onLoadSuccess={onDocumentLoadSuccess}>
-						{Array.from(
-							new Array(numPages),
-							(el,index) => (
-								<Page key={`page_${index+1}`} pageNumber={index+1} />
-							)
-						)}
+				<div>
+					<Document 
+						file={pdf} 
+						onLoadSuccess={onDocumentLoadSuccess} 
+						error={<div><img src={Frog} alt='frog'/><h3>Something went wrong and now you see the frog.</h3></div>}
+						loading={<h3>Loading...</h3>}
+						noData={<div><img src={Frog} alt='frog'/><h3>Something went wrong and now you see the frog.</h3></div>}
+						renderMode={'svg'}>
+						{Array.from(new Array(numPages), (el,index) => (
+							<Page 
+								key={`page_${index+1}`} 
+								pageNumber={index+1} 
+								error={<div><img src={Frog} alt='frog'/><h3>Something went wrong and now you see the frog.</h3></div>}
+								loading={<h3>Loading...</h3>}
+								noData={<div><img src={Frog} alt='frog'/><h3>Something went wrong and now you see the frog.</h3></div>}
+								renderMode={'svg'}
+								scale={1.7}
+							/>))}
 					</Document>
 				</div>
 				<p></p><a href={'https://github.com/lenferdetroud/lenferdetroud.github.io/raw/master/src/pages/posts/' + keyString + '.pdf'}>Download PDF</a>

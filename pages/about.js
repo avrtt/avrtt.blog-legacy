2deac5379b6ef94c41ea3ee771fb20e15b48a71a
avrtt <avrtt@tuta.io>
2024-08-27 11:41:55 +0500

    Updates

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+
+const About = () => {
+  return (
+    <main>
+      <h1>Gatsby component testing...</h1>
+      <p>(__gsql)</p>
+    </main>
+  )
+}
+
+export const Head = () => <title>About</title>
+
+export default About
